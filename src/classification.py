d8c5e57842e5bd1cd070ca94a75e216a36e92af2
xhuang1994 <xhuang1994@gmail.com>
2016-03-23 23:14:30 -0600

    client.py divided into data.py and classification.py

---
+++ src/classification.py
@@ -0,0 +1,308 @@
+import re
+from sklearn.linear_model import LogisticRegressionCV, LogisticRegression
+from sklearn import cross_validation
+from sklearn.cross_validation import KFold
+import numpy as np
+import csv
+from sklearn.cross_validation import train_test_split
+import matplotlib.pyplot as plt
+
+#This file reads data from polluters.txt and legitimate_users.txt and classify the records
+
+
+#read data from polluters.txt and legitimate_users.txt
+polluters = []
+reader_1 = open("polluters.txt", 'r')
+for line in reader_1:
+    tokens = [float(r) for r in re.split("[\t\n]", line)[:23]]
+    polluters.append(tokens)
+
+legitimate_users = []
+reader_2 = open("legitimate_users.txt", 'r')
+for line in reader_2:
+    tokens = [float(r) for r in re.split("[\t\n]", line)[:23]]
+    legitimate_users.append(tokens)
+
+
+#some index numbers of specific information for future use
+userID = 0
+numberOfFollowings = 1
+numberOfFollowers = 2
+numberOfTweets = 3
+lengthOfScreenName = 4
+lengthOfDescriptionInUserProfile = 5
+standard_deviation_diff = 6
+lag1_autocorrelation = 7
+number_tweets_Monday = 8
+#omited index numbers for number of tweets posted each day Tuesday - Saturday
+number_tweets_Sunday = 14
+ratio_tweets_Monday = 15
+#omited index numbers for ratio of tweets posted each day Tuesday - Saturday
+ratio_tweets_Sunday = 21
+ratio_urls_tweets = 22
+
+
+#combine 2 datasets for classification, dataset_Y is the target list
+#the data will be shuffled before classify
+dataset_X = polluters + legitimate_users
+dataset_Y = [0] * len(polluters) + [1] * len(legitimate_users)
+
+
+dataset_X_numberOfFollowings = [x[1] for x in dataset_X]
+dataset_X_numberOfFollowers = [x[2] for x in dataset_X]
+dataset_X_numberOfTweets = [x[3] for x in dataset_X]
+dataset_X_lengthOfScreenName = [x[4] for x in dataset_X]
+dataset_X_lengthOfDescriptionInUserProfile = [x[5] for x in dataset_X]
+dataset_X_standard_deviation_diff = [r[6] for r in dataset_X]
+dataset_X_lag1_autocorrelation = [r[7] for r in dataset_X]
+dataset_X_number_tweets_weekdays = [r[8:15] for r in dataset_X]
+dataset_X_ratio_tweets_weekdays = [r[15:22] for r in dataset_X]
+dataset_X_ratio_urls_tweets = [r[22] for r in dataset_X]
+
+
+#plot distribution graphs for each feature
+labels = ["Number of followings", "Number of follwers", "Number of tweets", "Length of screen name", "Length of self description", "Standard deviation difference", "Lag1 autocorrelation", "Ratio of urls over tweets"]
+data = [dataset_X_numberOfFollowings, dataset_X_numberOfFollowers, dataset_X_numberOfTweets, dataset_X_lengthOfScreenName, dataset_X_lengthOfDescriptionInUserProfile, dataset_X_standard_deviation_diff, dataset_X_lag1_autocorrelation, dataset_X_ratio_urls_tweets]
+weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+"""
+i = 0
+for d in data:
+    plt.plot(d, dataset_Y, 'bo')
+    xmin, xmax, ymin, ymax = plt.axis()
+    plt.axis([xmin-xmax*0.05, xmax+xmax*0.05, -0.05, 1.05])
+    plt.xlabel(labels[i])
+    plt.show()
+    i += 1
+
+
+for d in list(range(7)):
+    dataset_X_number_tweets_d = [r[d] for r in dataset_X_number_tweets_weekdays]
+    plt.plot(dataset_X_number_tweets_d, dataset_Y, 'bo')
+    xmin, xmax, ymin, ymax = plt.axis()
+    plt.axis([xmin-xmax*0.05, xmax+xmax*0.05, -0.05, 1.05])
+    plt.xlabel("Number of tweets posted on " + weekdays[d] + "s")
+    plt.show()
+    dataset_X_ratio_tweets_d = [r[d] for r in dataset_X_ratio_tweets_weekdays]
+    plt.plot(dataset_X_number_tweets_d, dataset_Y, 'bo')
+    xmin, xmax, ymin, ymax = plt.axis()
+    plt.axis([xmin-xmax*0.05, xmax+xmax*0.05, -0.05, 1.05])
+    plt.xlabel("Ratio of tweets posted on " + weekdays[d] + "s")
+    plt.show()
+
+
+#plot CDF functions for each feature
+#this method doesn't seem to work correctly, another method is used below
+num_bins = 20
+counts_1, bin_edges_1 = np.histogram(dataset_X_numberOfFollowings[:len(polluters)], bins = num_bins, normed = True)
+cdf_1 = np.cumsum(counts_1)
+print(bin_edges_1[:10])
+print(cdf_1[:10])
+counts_2, bin_edges_2 = np.histogram(dataset_X_numberOfFollowings[len(polluters):], bins = num_bins, normed = True)
+cdf_2 = np.cumsum(counts_2)
+plt.plot(bin_edges_1[1:] + bin_edges_2[1:], cdf_1 + cdf_2)
+plt.show()
+
+
+#second alternative method to plot CDF functions
+i = 0
+for d in data:
+    sorted_data_1 = list(np.sort(d[:len(polluters)]))
+    yvals_1 = list(np.arange(len(sorted_data_1))/float(len(sorted_data_1)))
+    sorted_data_2 = list(np.sort(d[len(polluters):]))
+    yvals_2 = list(np.arange(len(sorted_data_2))/float(len(sorted_data_2)))
+    plt.plot(sorted_data_1, yvals_1, 'b')
+    plt.plot(sorted_data_2, yvals_2, 'g')
+    xmin, xmax, ymin, ymax = plt.axis()
+    plt.axis([xmin-xmax*0.05, xmax+xmax*0.05, -0.05, 1.05])
+    plt.xlabel("CDF - " + labels[i])
+    plt.show()
+    i += 1
+
+for d in list(range(7)):
+    dataset_X_number_tweets_d = [r[d] for r in dataset_X_number_tweets_weekdays]
+    sorted_data_1 = list(np.sort(dataset_X_number_tweets_d[:len(polluters)]))
+    yvals_1 = list(np.arange(len(sorted_data_1))/float(len(sorted_data_1)))
+    sorted_data_2 = list(np.sort(dataset_X_number_tweets_d[len(polluters):]))
+    yvals_2 = list(np.arange(len(sorted_data_2))/float(len(sorted_data_2)))
+    plt.plot(sorted_data_1, yvals_1, 'b')
+    plt.plot(sorted_data_2, yvals_2, 'g')
+    xmin, xmax, ymin, ymax = plt.axis()
+    plt.axis([xmin-xmax*0.05, xmax+xmax*0.05, -0.05, 1.05])
+    plt.xlabel("CDF - Number of tweets posted on " + weekdays[d] + "s")
+    plt.show()
+    
+    dataset_X_ratio_tweets_d = [r[d] for r in dataset_X_ratio_tweets_weekdays]
+    sorted_data_1 = list(np.sort(dataset_X_ratio_tweets_d[:len(polluters)]))
+    yvals_1 = list(np.arange(len(sorted_data_1))/float(len(sorted_data_1)))
+    sorted_data_2 = list(np.sort(dataset_X_ratio_tweets_d[len(polluters):]))
+    yvals_2 = list(np.arange(len(sorted_data_2))/float(len(sorted_data_2)))
+    plt.plot(sorted_data_1, yvals_1, 'b')
+    plt.plot(sorted_data_2, yvals_2, 'g')
+    xmin, xmax, ymin, ymax = plt.axis()
+    plt.axis([xmin-xmax*0.05, xmax+xmax*0.05, -0.05, 1.05])
+    plt.xlabel("CDF - Ratio of tweets posted on " + weekdays[d] + "s")
+    plt.show()
+"""
+
+
+#need KFold iterator since method "cross_val_score" doesn't provide shuffling function
+kFold = KFold(n = len(dataset_X), n_folds = 10, shuffle = True)
+
+#build the classifier and do classification based on: all numeric features & each numeric feature
+lr = LogisticRegression()
+
+"""
+#randomly separate data into training (80%) and test (20%) sets
+#call lr.fit() and lr.predict() and compare the prediction versus real classes
+#print instances that are incorrectly classified
+X_train, X_test, y_train, y_test = train_test_split(dataset_X_all_numeric_features, dataset_Y, test_size = 0.2, random_state = 0)
+lr.fit(X_train, y_train)
+prediction = lr.predict(X_test)
+i = 0
+e = 0
+while i < len(X_test):
+    if prediction[i] != y_test[i]:
+        e += 1
+        print(X_test[i])
+    i += 1
+print("# errors: ", e)
+print("# total instances: ", len(X_test))
+
+
+#number of iterations (6)
+n = list(range(6))
+
+dataset_X_all_numeric_features = [r[1:] for r in dataset_X]
+scores_all_numeric_features = 0
+for i in n:
+    scores_all_numeric_features += np.mean(cross_validation.cross_val_score(lr, dataset_X_all_numeric_features, dataset_Y, cv = kFold))
+print("All numeric features: ", scores_all_numeric_features / len(n))
+
+scores_numberOfFollowings = 0
+for i in n:
+    scores_numberOfFollowings += np.mean(cross_validation.cross_val_score(lr, dataset_X_numberOfFollowings, dataset_Y, cv = kFold))
+print("Number of followings: ", scores_numberOfFollowings / len(n))
+
+scores_numberOfFollowers = 0
+for i in n:
+    scores_numberOfFollowers += np.mean(cross_validation.cross_val_score(lr, dataset_X_numberOfFollowers, dataset_Y, cv = kFold))
+print("Number of followers: ", scores_numberOfFollowers / len(n))
+
+scores_numberOfTweets = 0
+for i in n:
+    scores_numberOfTweets += np.mean(cross_validation.cross_val_score(lr, dataset_X_numberOfTweets, dataset_Y, cv = kFold))
+print("Number of tweets: ", scores_numberOfTweets / len(n))
+
+scores_lengthOfScreenName = 0
+for i in n:
+    scores_lengthOfScreenName += np.mean(cross_validation.cross_val_score(lr, dataset_X_lengthOfScreenName, dataset_Y, cv = kFold))
+print("Length of screen name: ", scores_lengthOfScreenName / len(n))
+
+scores_lengthOfDescriptionInUserProfile = 0
+for i in n:
+    scores_lengthOfDescriptionInUserProfile += np.mean(cross_validation.cross_val_score(lr, dataset_X_lengthOfDescriptionInUserProfile, dataset_Y, cv = kFold))
+print("Length of description in user profile: ", scores_lengthOfDescriptionInUserProfile / len(n))
+
+scores_standard_deviation_diff = 0
+for i in n:
+    scores_standard_deviation_diff += np.mean(cross_validation.cross_val_score(lr, dataset_X_standard_deviation_diff, dataset_Y, cv = kFold))
+print("standard deviation diff: ", scores_standard_deviation_diff / len(n))
+
+scores_lag1_autocorrelation = 0
+for i in n:
+    scores_lag1_autocorrelation += np.mean(cross_validation.cross_val_score(lr, dataset_X_lag1_autocorrelation, dataset_Y, cv = kFold))
+print("lag1 autocorrelation: ", scores_lag1_autocorrelation / len(n))
+
+scores_number_tweets_weekdays = 0
+for i in n:
+    scores_number_tweets_weekdays += np.mean(cross_validation.cross_val_score(lr, dataset_X_number_tweets_weekdays, dataset_Y, cv = kFold))
+print("Number of tweets each weekday: ", scores_number_tweets_weekdays / len(n))
+
+scores_ratio_tweets_weekdays = 0
+for i in n:
+    scores_ratio_tweets_weekdays += np.mean(cross_validation.cross_val_score(lr, dataset_X_ratio_tweets_weekdays, dataset_Y, cv = kFold))
+print("Ratio of tweets each weekday: ", scores_ratio_tweets_weekdays / len(n))
+
+scores_ratio_urls_tweets = 0
+for i in n:
+    scores_ratio_urls_tweets += np.mean(cross_validation.cross_val_score(lr, dataset_X_ratio_urls_tweets, dataset_Y, cv = kFold))
+print("Ratio of urls over tweets: ", scores_ratio_urls_tweets / len(n))
+
+print("\n****************************************************\n")
+
+dataset_X_no_numberOfFollowings = [x[2:] for x in dataset_X]
+scores_no_numberOfFollowings = 0
+for i in n:
+    scores_no_numberOfFollowings += np.mean(cross_validation.cross_val_score(lr, dataset_X_no_numberOfFollowings, dataset_Y, cv = kFold))
+print("No number of followings: ", scores_no_numberOfFollowings / len(n))
+
+dataset_X_no_numberOfFollowers = [[x[1]] + x[3:] for x in dataset_X]
+scores_no_numberOfFollowers = 0
+for i in n:
+    scores_no_numberOfFollowers += np.mean(cross_validation.cross_val_score(lr, dataset_X_no_numberOfFollowers, dataset_Y, cv = kFold))
+print("No number of followers: ", scores_no_numberOfFollowers / len(n))
+
+dataset_X_no_numberOfTweets = [x[1:3] + x[4:] for x in dataset_X]
+scores_no_numberOfTweets = 0
+for i in n:
+    scores_no_numberOfTweets += np.mean(cross_validation.cross_val_score(lr, dataset_X_no_numberOfTweets, dataset_Y, cv = kFold))
+print("No number of tweets: ", scores_no_numberOfTweets / len(n))
+
+dataset_X_no_lengthOfScreenName = [x[1:4] + x[5:] for x in dataset_X]
+scores_no_lengthOfScreenName = 0
+for i in n:
+    scores_no_lengthOfScreenName += np.mean(cross_validation.cross_val_score(lr, dataset_X_no_lengthOfScreenName, dataset_Y, cv = kFold))
+print("No length of screen name: ", scores_no_lengthOfScreenName / len(n))
+
+dataset_X_no_lengthOfDescriptionInUserProfile = [x[1:5] + x[6:] for x in dataset_X]
+scores_no_lengthOfDescriptionInUserProfile = 0
+for i in n:
+    scores_no_lengthOfDescriptionInUserProfile += np.mean(cross_validation.cross_val_score(lr, dataset_X_no_lengthOfDescriptionInUserProfile, dataset_Y, cv = kFold))
+print("No length of description in user profile: ", scores_no_lengthOfDescriptionInUserProfile / len(n))
+
+dataset_X_no_standard_deviation_diff = [r[1:6] + r[7:] for r in dataset_X]
+scores_no_standard_deviation_diff = 0
+for i in n:
+    scores_no_standard_deviation_diff += np.mean(cross_validation.cross_val_score(lr, dataset_X_no_standard_deviation_diff, dataset_Y, cv = kFold))
+print("no standard deviation diff: ", scores_no_standard_deviation_diff / len(n))
+
+dataset_X_no_lag1_autocorrelation = [r[1:7] + r[8:] for r in dataset_X]
+scores_no_lag1_autocorrelation = 0
+for i in n:
+    scores_no_lag1_autocorrelation += np.mean(cross_validation.cross_val_score(lr, dataset_X_no_lag1_autocorrelation, dataset_Y, cv = kFold))
+print("no lag1 autocorrelation: ", scores_no_lag1_autocorrelation / len(n))
+
+dataset_X_no_number_tweets_weekdays = [r[1:9] + r[15:] for r in dataset_X]
+scores_no_number_tweets_weekdays = 0
+for i in n:
+    scores_no_number_tweets_weekdays += np.mean(cross_validation.cross_val_score(lr, dataset_X_no_number_tweets_weekdays, dataset_Y, cv = kFold))
+print("no number of tweets each weekday: ", scores_no_number_tweets_weekdays / len(n))
+
+dataset_X_no_ratio_tweets_weekdays = [r[1:15] + [r[22]] for r in dataset_X]
+scores_no_ratio_tweets_weekdays = 0
+for i in n:
+    scores_no_ratio_tweets_weekdays += np.mean(cross_validation.cross_val_score(lr, dataset_X_no_ratio_tweets_weekdays, dataset_Y, cv = kFold))
+print("no ratio of tweets each weekday: ", scores_no_ratio_tweets_weekdays / len(n))
+
+dataset_X_no_ratio_urls_tweets = [r[1:22] for r in dataset_X]
+scores_no_ratio_urls_tweets = 0
+for i in n:
+    scores_no_ratio_urls_tweets += np.mean(cross_validation.cross_val_score(lr, dataset_X_no_ratio_urls_tweets, dataset_Y, cv = kFold))
+print("no ratio of urls over tweets: ", scores_no_ratio_urls_tweets / len(n))
+
+print("\n****************************************************\n")
+print("Coefficients: \n")
+
+lr.fit(dataset_X_all_numeric_features, dataset_Y)
+coef = lr.coef_[0]
+print("Number of followings: ", coef[0])
+print("Number of followers: ", coef[1])
+print("Number of tweets: ", coef[2])
+print("Length of screen name: ", coef[3])
+print("Length of description in user profile: ", coef[4])
+print("Standard deviation differences: ", coef[5])
+print("Lag one autocorrelation: ", coef[6])
+print("Number of tweets each weekday (avg): ", np.mean(coef[7:14]))
+print("Ratio of tweets each weekday (avg): ", np.mean(coef[14:21]))
+print("Ratio of urls over tweets: ", coef[21])
+"""
---
+++ src/data.py
@@ -0,0 +1,246 @@
+import re
+import numpy as np
+from Lib import statistics as stat
+import datetime as dt
+
+#This file reads data from honeypot dataset, process and write the data into polluters.txt and legitimate_users.txt
+
+
+#read the text file of polluters information
+reader_1 = open("social_honeypot_icwsm_2011\content_polluters.txt", 'r')
+
+#convert the content read into usable dataset
+polluters = []
+for line in reader_1:
+    tokens = re.split("[\t\n]", line)
+    #parse strings to numbers, take out the dates
+    parsed_tokens = [int(r) for r in ([tokens[0]] + tokens[3:8])]
+    polluters.append(parsed_tokens)
+reader_1.close()
+
+#read the file of legitimate users information, and convert it into usable dataset
+reader_2 = open("social_honeypot_icwsm_2011\legitimate_users.txt", 'r')
+legitimate_users = []
+for line in reader_2:
+    tokens = re.split("[\t\n]", line)
+    parsed_tokens = [int(r) for r in ([tokens[0]] + tokens[3:8])]
+    legitimate_users.append(parsed_tokens)
+reader_2.close()
+
+#read text file containing series of number of followings for each bot user
+reader_3 = open("social_honeypot_icwsm_2011\content_polluters_followings.txt", 'r')
+
+#convert the contend read into usable dataset
+i = 0
+for line in reader_3:
+    tokens = [int(r) for r in re.split("[\D]", line) if r != ""][1:]
+    #calculate standard deviation of the differences of the data
+    sd = stat.pstdev(tokens)
+    #calculate lag one autocorrelation
+    #ac = np.correlate(tokens[1:len(tokens)], tokens[:len(tokens) - 1])  """I can't find what I need from output from this function as a single number"""
+    avg = np.mean(tokens)
+    j = 0
+    numerator = 0
+    denominator = (tokens[0] - avg) ** 2
+    while j < len(tokens) - 1:
+        numerator += (tokens[j] - avg) * (tokens[j+1] - avg)
+        denominator += (tokens[j+1] - avg) ** 2
+        j += 1
+    if denominator == 0:
+        ac = 0
+    else:
+        ac = numerator / denominator
+    polluters[i].append(sd)
+    polluters[i].append(ac)
+    i += 1
+reader_3.close()
+
+#read text file containing series of number of followings for each human user
+reader_4 = open("social_honeypot_icwsm_2011\legitimate_users_followings.txt", 'r')
+
+#convert the contend read into usable dataset
+i = 0
+for line in reader_4:
+    tokens = [int(r) for r in re.split("[\D]", line) if r != ""][1:]
+    #calculate standard deviation of the differences of the data
+    sd = stat.pstdev(tokens)
+    #calculate lag one autocorrelation
+    avg = np.mean(tokens)
+    j = 0
+    numerator = 0
+    denominator = (tokens[0] - avg) ** 2
+    while j < len(tokens) - 1:
+        numerator += (tokens[j] - avg) * (tokens[j+1] - avg)
+        denominator += (tokens[j+1] - avg) ** 2
+        j += 1
+    if denominator == 0:
+        ac = 0
+    else:
+        ac = numerator / denominator
+    legitimate_users[i].append(sd)
+    legitimate_users[i].append(ac)
+    i += 1
+reader_4.close()
+
+#read the text file of tweets posted by each bot user
+reader_5 = open("social_honeypot_icwsm_2011\content_polluters_tweets.txt", encoding = 'utf-8', mode = 'r')
+
+urls = []                   #collection of urls from tweets
+#convert the contend read into usable dataset
+prev_userID = 0
+curr_userID = 0
+curr_tweetIDs = []          #collection of ids of tweets posted by current user
+curr_tweets = []            #collection of tweets posted by current user
+curr_tweets_created_at = [] #create dates of tweets
+curr_tweets_weekday = []    #weekdays of create dates of tweets
+urls_tweet = []             #number of urls from tweets of current user
+polluter_ids = [s[0] for s in polluters]
+curr_line = reader_5.readline()
+while 1:
+    prev_userID = curr_userID
+    if curr_line != '':
+        tokens = [r for r in re.split("[\t\n]", curr_line) if r != ""]
+        #the date the tweet was posted
+        date_info = re.split("[-\s]", tokens[3])
+        post_date = dt.date(int(date_info[0]), int(date_info[1]), int(date_info[2]))
+
+        curr_userID = int(tokens[0])
+    #new user found / end of the file
+    if prev_userID != 0 and curr_userID != prev_userID or curr_line == '':
+        index = polluter_ids.index(prev_userID)
+        j = 0
+        while j < 7:
+            polluters[index].append(curr_tweets_weekday.count(j))
+            j += 1
+        k = 0
+        while k < 7:
+            polluters[index].append(curr_tweets_weekday.count(k) / len(curr_tweets_weekday))
+            k += 1
+        urls += urls_tweet
+        polluters[index].append(len(urls_tweet) / len(curr_tweets))
+        urls_tweet = []
+        curr_tweetIDs = []
+        curr_tweets = []
+        curr_tweets_created_at = []
+        curr_tweets_weekday = []
+        if curr_line == '':
+            break
+    urls_tweet += re.findall('http[\S]+', tokens[2])
+    curr_tweets.append(tokens[2])
+    curr_tweetIDs.append(int(tokens[1]))
+    curr_tweets_weekday.append(post_date.weekday())
+    curr_line = reader_5.readline()
+reader_5.close()
+
+
+#read the text file of tweets posted by each human user
+reader_6 = open("social_honeypot_icwsm_2011\legitimate_users_tweets.txt", encoding = 'utf-8', mode = 'r')
+
+#convert the contend read into usable dataset
+prev_userID = 0
+curr_userID = 0
+curr_tweetIDs = []          #collection of ids of tweets posted by current user
+curr_tweets = []            #collection of tweets posted by current user
+curr_tweets_created_at = [] #create dates of tweets
+curr_tweets_weekday = []    #weekdays of create dates of tweets
+urls_tweet = []             #number of urls from tweets of current user
+legitimate_user_ids = [s[0] for s in legitimate_users]
+curr_line = reader_6.readline()
+while 1:
+    prev_userID = curr_userID
+    if curr_line != '':
+        tokens = [r for r in re.split("[\t\n]", curr_line) if r != ""]
+        #the date the tweet was posted
+        date_info = re.split("[-\s]", tokens[3])
+        post_date = dt.date(int(date_info[0]), int(date_info[1]), int(date_info[2]))
+
+        curr_userID = int(tokens[0])
+    #new user found / end of the file
+    if prev_userID != 0 and curr_userID != prev_userID or curr_line == '':
+        index = legitimate_user_ids.index(prev_userID)
+        j = 0
+        while j < 7:
+            legitimate_users[index].append(curr_tweets_weekday.count(j))
+            j += 1
+        k = 0
+        while k < 7:
+            legitimate_users[index].append(curr_tweets_weekday.count(k) / len(curr_tweets_weekday))
+            k += 1
+        urls += urls_tweet
+        legitimate_users[index].append(len(urls_tweet) / len(curr_tweets))
+        urls_tweet = []
+        curr_tweetIDs = []
+        curr_tweets = []
+        curr_tweets_created_at = []
+        curr_tweets_weekday = []
+        if curr_line == '':
+            break
+    urls_tweet += re.findall('http[\S]+', tokens[2])
+    curr_tweets.append(tokens[2])
+    curr_tweetIDs.append(int(tokens[1]))
+    curr_tweets_weekday.append(post_date.weekday())
+    curr_line = reader_6.readline()
+reader_6.close()
+
+
+#Found 44 ambiguous users who are in both polluters and legitimate users, delete those users
+#The user ids are found in ascending order
+i = 0
+j = 0
+count = 0
+while i < len(polluters) and j < len(legitimate_users):
+    if polluters[i][0] == legitimate_users[j][0]:
+        polluters.pop(i)
+        legitimate_users.pop(j)
+        count += 1
+    elif polluters[i][0] < legitimate_users[j][0]:
+        i += 1
+    else:
+        j += 1
+print("%d users deleted!" % count)
+
+#add 0's for missing values (some users have no tweets recorded)
+i = 0
+while i < len(polluters):
+    if len(polluters[i]) < 23:
+        polluters[i] += [0] * 15
+    i += 1
+j = 0
+while j < len(legitimate_users):
+    if len(legitimate_users[j]) < 23:
+        legitimate_users[j] += [0] * 15
+    j += 1
+
+
+#write collected & processed data into files
+fwriter_1 = open('polluters.txt', 'w')
+for record in polluters:
+    s = '\t'.join([str(r) for r in record]) + '\n'
+    fwriter_1.write(s)
+fwriter_1.close()
+print("data written to polluters.txt")
+
+fwriter_2 = open('legitimate_users.txt', 'w')
+for record in legitimate_users:
+    s = '\t'.join([str(r) for r in record]) + '\n'
+    fwriter_2.write(s)
+fwriter_2.close()
+print("data written to legitimate_users.txt")
+
+
+#some index numbers of specific information for future use
+userID = 0
+numberOfFollowings = 1
+numberOfFollowers = 2
+numberOfTweets = 3
+lengthOfScreenName = 4
+lengthOfDescriptionInUserProfile = 5
+standard_deviation_diff = 6
+lag1_autocorrelation = 7
+number_tweets_Monday = 8
+#omited index numbers for number of tweets posted each day Tuesday - Saturday
+number_tweets_Sunday = 14
+ratio_tweets_Monday = 15
+#omited index numbers for ratio of tweets posted each day Tuesday - Saturday
+ratio_tweets_Sunday = 21
+ratio_urls_tweets = 22
